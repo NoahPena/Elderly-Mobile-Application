60f84370fc5b41516c73abe8bfcabad41f75ac38
NoahPena <noahpena7@gmail.com>
2016-11-30 03:54:07 -0600

    Added body-parser

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded(
+{
+	extended: true
+}));
 
 app.get('/', function(request, response)
 {
@@ -12,6 +18,7 @@ app.get('/', function(request, response)
 app.post('/', function(request, response)
 {
 	console.log(request.body);
+	response.send("POST Received");
 });
 
 app.listen(app.get('port'), function()
@@ -19,38 +26,5 @@ app.listen(app.get('port'), function()
 		console.log("Now listening on port " + app.get('port'));
 	});
 
-// //Lets require/import the HTTP module
-// var http = require('http');
-
-// //Lets define a port we want to listen to
-// const PORT=8080; 
-
-// //We need a function which handles requests and send response
-// function handleRequest(request, response)
-// {
-// 	response.writeHead(200, {"Content-Type":"text\plain"});
-
-// 	if(request.method == "GET")
-//     {
-//             response.end("received GET request.")
-//     }
-//     else if(request.method == "POST")
-//     {
-//             response.end("received POST request.");
-//     }
-//     else
-//     {
-//             response.end("Undefined request .");
-//     }
-// }
-
-// //Create a server
-// var server = http.createServer(handleRequest);
-
-// //Lets start our server
-// server.listen(process.env.PORT || PORT, function(){
-//     //Callback triggered when server is successfully listening. Hurray!
-//     console.log("Server listening on: http://localhost:%s", process.env.PORT || PORT);
-// });
 
 //LAT=27.714518 LON=-97.328522 SAT=3 PREC=375 CHARS=3227261 SENTENCES=6399 CSUM ERR=1
