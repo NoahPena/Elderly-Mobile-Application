e7aadfe8bb096cd54e576a83717983b2446e3e82
NoahPena <noahpena7@gmail.com>
2016-11-30 00:20:14 -0600

    Added POST and GET callbacks

--- index.js
+++ index.js
@@ -5,8 +5,33 @@ var http = require('http');
 const PORT=8080; 
 
 //We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
+function handleRequest(request, response)
+{
+	console.dir(request.param);
+
+    if (request.method == 'POST') {
+        console.log("POST");
+        var body = '';
+        request.on('data', function (data) {
+            body += data;
+            console.log("Partial body: " + body);
+        });
+        request.on('end', function () {
+            console.log("Body: " + body);
+        });
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end('post received');
+    }
+    else
+    {
+        console.log("GET");
+        //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
+        var html = fs.readFileSync('index.html');
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(html);
+    }
+
+    //response.end('It Works!! Path Hit: ' + request.url);
 }
 
 //Create a server
